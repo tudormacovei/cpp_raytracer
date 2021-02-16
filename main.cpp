7db53f508b6e9f6c0a0a9308820a69638b4caf1b
Macovei Tudor <tudor_macovei@iCloud.com>
2021-02-16 18:47:51 +0100

    add sphere render with normal shading

--- main.cpp
+++ main.cpp
@@ -1,27 +1,61 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
-using namespace std;
+#include "vec3.h"
+#include "ray.h"
+
+
+float hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    // quadratic eq. from the vectors of the center of the sphere, the radius, and the vectors
+    // that define the ray, the discriminant is a parameter t such ray(t) is a point on the sphere
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return ((-b - sqrt(discriminant)) / (2.0 * a));
+    }
+}
+
+vec3 color(const ray &r) {
+    float t = hit_sphere(vec3(0,0,-2), 1, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector((r.point_at_parameter(t)) - vec3(0, 0, -2)); //normal
+        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) *vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main() {
     int nx;
     int ny;
-    ofstream fout("render.ppm");
+    std::ofstream fout("render.ppm");
 
     nx = 200;
     ny = 100;
 
     fout << "P3\n" << nx << " " << ny << "\n255\n";
+
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
     // rows top to bot
     for (int j = ny - 1; j >= 0; j--) {
-        // each row left to right
         for (int i = 0; i < nx; i++) {
-            float r = float (i) / float(nx);
-            float g = float (j) / float(ny);
-            float b = 0.2;
-            int ir = int (255.99 * r);
-            int ig = int (255.99 * g);
-            int ib = int (255.99 * b);
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            vec3 col = color(r);
+            int ir = int(255.99 * col.r());
+            int ig = int(255.99 * col.g());
+            int ib = int(255.99 * col.b());
             fout << ir << " " << ig << " " << ib << "\n";
         }
     }
--- vec3.h
+++ vec3.h
@@ -2,8 +2,8 @@
 // Created by tudor on 16/02/2021.
 //
 
-#ifndef UNTITLED_VEC3_H
-#define UNTITLED_VEC3_H
+#ifndef VEC3_H
+#define VEC3_H
 
 
 #include <cmath>
@@ -13,47 +13,172 @@ private:
     float e[3];
 public:
     vec3() {}
-    vec3 (float e0, float e1, float e2) {
+
+    vec3(float e0, float e1, float e2) {
         e[0] = e0;
         e[1] = e1;
         e[2] = e2;
     }
-    inline void set_x (float x) { e[0] = x; };
-    inline void set_y (float x) { e[1] = x; };
-    inline void set_z (float x) { e[2] = x; };
+
+    inline void set_x(float x) { e[0] = x; };
+
+    inline void set_y(float x) { e[1] = x; };
+
+    inline void set_z(float x) { e[2] = x; };
+
     // when using class for vectors
     inline float x() const { return e[0]; }
+
     inline float y() const { return e[1]; }
+
     inline float z() const { return e[2]; }
+
     // when using class for color
     inline float r() const { return e[0]; }
+
     inline float g() const { return e[1]; }
+
     inline float b() const { return e[2]; }
 
-    inline const vec3& operator+() const { return *this; }
-    inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    inline float operator[] (int i) const { return e[i]; }
-    inline float& operator[](int i) { return e[i]; }
+    inline const vec3 &operator+() const { return *this; }
 
-    inline vec3& operator+=(const vec3 &v2);
-    inline vec3& operator-=(const vec3 &v2);
-    inline vec3& operator*=(const vec3 &v2);
-    inline vec3& operator/=(const vec3 &v2);
-    inline vec3& operator*=(const float t);
-    inline vec3& operator/=(const float t);
+    inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+
+    inline float operator[](int i) const { return e[i]; }
+
+    inline float &operator[](int i) { return e[i]; }
+
+    inline vec3 &operator+=(const vec3 &v2);
+
+    inline vec3 &operator-=(const vec3 &v2);
+
+    inline vec3 &operator*=(const vec3 &v2);
+
+    inline vec3 &operator/=(const vec3 &v2);
+
+    inline vec3 &operator*=(const float t);
+
+    inline vec3 &operator/=(const float t);
 
     inline float length() const {
-        return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+        return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
     }
 
     inline float squared_length() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 
     inline void make_unit_vector() {
         float k = 1.0 / squared_length();
+        e[0] *= k;
+        e[1] *= k;
+        e[2] *= k;
     }
 };
 
+inline std::istream &operator>>(std::istream &is, vec3 &t) {
+    // e[3] is not a public variable
+    float temp;
+    is >> temp;
+    t.set_x(temp);
+    is >> temp;
+    t.set_y(temp);
+    is >> temp;
+    t.set_z(temp);
+    return is;
+}
+
+inline std::ostream &operator<<(std::ostream &os, const vec3 &t) {
+    os << t.x() << " " << t.y() << " " << t.z();
+    return os;
+}
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z());
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z());
+}
+
+inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x() * v2.x(), v1.y() * v2.y(), v1.z() * v2.z());
+}
+
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x() / v2.x(), v1.y() / v2.y(), v1.z() / v2.z());
+}
+
+inline vec3 operator/(const vec3 &v, float a) {
+    return vec3(v.x() / a, v.y() / a, v.z() / a);
+}
+
+inline vec3 operator*(const vec3 &v, float a) {
+    return vec3(a * v.x(), a * v.y(), a * v.z());
+}
+
+// inverted positions, same as function above
+
+inline vec3 operator*(float a, const vec3 &v) {
+    return vec3(a * v.x(), a * v.y(), a * v.z());
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+    return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+    return vec3((v1.y() * v2.z() - v1.z() * v2.y()),
+                (-(v1.x() * v2.z() - v1.z() * v2.x())),
+                (v1.x() * v2.y() - v1.y() * v2.x()));
+}
+
+inline vec3 &vec3::operator+=(const vec3 &v) {
+    e[0] += v.x();
+    e[1] += v.y();
+    e[2] += v.z();
+    return *this;
+}
+
+inline vec3 &vec3::operator*=(const vec3 &v) {
+    e[0] *= v.x();
+    e[1] *= v.y();
+    e[2] *= v.z();
+    return *this;
+}
+
+inline vec3 &vec3::operator*=(const float a) {
+    e[0] *= a;
+    e[1] *= a;
+    e[2] *= a;
+    return *this;
+}
+
+inline vec3 &vec3::operator/=(const vec3 &v) {
+    e[0] /= v.x();
+    e[1] /= v.y();
+    e[2] /= v.z();
+    return *this;
+}
+
+inline vec3 &vec3::operator/=(const float t) {
+    float k = 1.0 / t;
+
+    e[0] *= k;
+    e[1] *= k;
+    e[2] *= k;
+    return *this;
+}
+
+inline vec3 &vec3::operator-=(const vec3 &v) {
+    e[0] -= v.x();
+    e[1] -= v.y();
+    e[2] -= v.z();
+    return *this;
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
 
 #endif //UNTITLED_VEC3_H
